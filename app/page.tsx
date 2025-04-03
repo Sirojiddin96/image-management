922eccd6631714ec0d8fd95dd0c0a6b464261864
backend-sirojiddin <Sirojiddin@medit.com>
2025-04-03 23:09:06 +0900

    first commit and project setup with image upload and list

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,82 @@
+'use client';
+import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const [images, setImage] = useState<File[]>([]);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <main className="flex min-h-[100vh] flex-col items-start justify-start p-4">
+      <div className="flex flex-col items-center justify-center w-full gap-2">
+        <div className="flex flex-row items-end justify-between w-full">
+          <div>
+            <h1 className="text-2xl font-bold mb-4">Image Management Tool</h1>
+            <p className="mb-4">
+              Upload an image to compress it. The compressed image will be saved in the same directory.
+            </p>
+          </div>
+          <div className="flex flex-col items-end">
+            <input
+              className="border-2 border-gray-300 p-2 rounded-lg display-none"
+              multiple
+              type="file"
+              accept="image/*"
+              title="Choose an image file"
+              onChange={(e) => {
+                console.log(e.target.files);
+                const files = e.target.files;
+                if (files && files[0]) {
+                  const file = files[0];
+                  console.log(file);
+                  setImage(prev => [...prev, file]);
+                }
+              }}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="w-full flex flex-row gap-2 items-center p-4 rounded-lg border-2 border-gray-300">
+          {images.length > 0 ?
+            images.map((_, index) => (
+              <div key={index} className=" h-40 bg-gray-200 flex items-center justify-center mb-4">
+                {(() => {
+                  const objectUrl = URL.createObjectURL(images[index]);
+                  console.log(objectUrl);
+                  return (
+                    <Image
+                      src={objectUrl}
+                      alt="Uploaded Image"
+                      width={150}
+                      height={150}
+                      className="rounded-lg"
+                      onLoad={() => URL.revokeObjectURL(objectUrl)}
+                    />
+                  );
+                })()}
+              </div>
+            )) :
+            <div className="h-100 bg-gray-200 flex items-center justify-center w-full">
+              <span className="text-gray-500">No images uploaded yet</span>
+            </div>
+          }
+        </div>
+        {images.length > 0 && <div className="flex items-center justify-center w-full mt-4">
+          <button
+            type="button"
+            className="px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400 cursor-pointer"
+            onClick={() => console.log("Previous Page")}
+          >
+            Previous
+          </button>
+          <span className="mx-4 text-gray-700">Page 1 of 10</span>
+          <button
+            type="button"
+            className="px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400 cursor-pointer"
+            onClick={() => console.log("Next Page")}
+          >
+            Next
+          </button>
+        </div>}
+      </div>
+
+    </main>
   );
 }
